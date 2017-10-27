9a3dc431cf7cafb76b79dc2a2ce9fc6115a5880f
pomber <pombopombopombo@gmail.com>
2017-10-27 01:10:09 -0300

    Hack dependencies

--- task.js
+++ task.js
@@ -1,6 +1,8 @@
+"use strict";
+
 const Twit = require("twit");
-const markov = require("hx-markov-chain");
-const tokenizer = require("hx-tokenizer");
+const markov = requireMarkov();
+const tokenizer = requireTokenizer();
 const BOT_NAME = "sanatabot";
 
 const queries = [
@@ -225,4 +227,178 @@ function getGoodReply(twitter, username) {
     }
     return sanitizeReply(reply);
   });
+}
+
+// Fix:
+// Provisioning 3 modules...
+// hx-markov-chain@1.2.0 is failed
+// hx-tokenizer@1.0.0 is failed
+// twit@2.2.9 is available
+// The module hx-markov-chain@1.2.0 failed to build.
+// error Command failed with exit code 7.
+
+function requireMarkov() {
+  var START = " ^";
+  var END = " $";
+  var DEPTH = 3;
+
+  function create() {
+    return { count: 0, tokens: {} };
+  }
+
+  function update(model, chain) {
+    var depth = DEPTH;
+    var fullChain = [].concat(START, chain, END);
+    for (var i = 1; i <= fullChain.length; i++) {
+      var start = i < depth ? 0 : i - depth;
+      var end = i;
+      var currentChain = fullChain.slice(start, end);
+      add(model, currentChain);
+    }
+  }
+
+  function run(model) {
+    var chain = [START];
+    var token = START;
+    while (token != END) {
+      var prev = chain.slice(-DEPTH + 1);
+      var token = next(model, prev);
+      chain.push(token);
+    }
+    return chain.slice(1, -1);
+  }
+
+  function add(model, chain) {
+    var node = getNode(model, chain);
+    node.count += 1;
+    var next = chain.slice(1);
+
+    if (next.length) {
+      add(model, next);
+    } else {
+      model.count += 1;
+    }
+  }
+
+  function getNode(model, chain) {
+    var node = model;
+    chain.forEach(function(token) {
+      node.tokens[token] = node.tokens[token] || create();
+      node = node.tokens[token];
+    });
+    return node;
+  }
+
+  function count(model, chain) {
+    var node = getNode(model, chain);
+    return node.count;
+  }
+
+  function next(model, prev) {
+    var node = getNode(model, prev);
+    if (!node.count) {
+      throw Error("Path missing from model " + prev);
+    }
+    var pivot = Math.random() * node.count;
+    var tokens = Object.keys(node.tokens);
+    var i = 0;
+    for (var k = (i = 0); k < tokens.length; k++) {
+      var token = tokens[k];
+      i += node.tokens[token].count;
+      if (i > pivot) {
+        return token;
+      }
+    }
+  }
+
+  return {
+    START: START,
+    END: END,
+    create: create,
+    update: update,
+    count: count,
+    run: run
+  };
+}
+
+function requireTokenizer() {
+  function tokenize(text) {
+    var regexes = [
+      url,
+      hashtag,
+      mention,
+      smiley,
+      emoji,
+      acronyms,
+      punctuation,
+      words,
+      eol,
+      other
+    ];
+
+    var source = regexes
+      .map(function(re) {
+        return re.source;
+      })
+      .join("|");
+
+    var regex = new RegExp(source, "gi");
+
+    return text.match(regex);
+  }
+
+  function join(tokens) {
+    if (!tokens || !tokens.length) {
+      return "";
+    }
+
+    var text = tokens[0];
+    var openQuotes = {};
+    openQuotes[text] = true;
+
+    for (var i = 1; i < tokens.length; i++) {
+      var token = tokens[i];
+      var prev = tokens[i - 1];
+
+      var prevOpening = prev.match(quotes)
+        ? openQuotes[prev]
+        : prev.match(opening);
+      var thisClosing = token.match(quotes)
+        ? openQuotes[token]
+        : token.match(closing);
+      var sep = prevOpening || thisClosing ? "" : " ";
+      text += sep + token;
+
+      if (token.match(quotes)) {
+        openQuotes[token] = !openQuotes[token];
+      }
+    }
+
+    return text;
+  }
+
+  var tokenizer = {
+    tokenize: tokenize,
+    join: join
+  };
+
+  // Regexes:
+  var url = /[a-z]+:\/\/[a-z0-9-_]+.[a-z0-9-_:%&~?/.=]+/;
+  var hashtag = /[#]+[a-z0-9-_]+/;
+  var mention = /(^.)?\B@[a-z0-9_-]+/;
+  var smiley = /:\(|:\)|:P|:p|:O/;
+  var punctuation = /[.,;â€¦'Â¿Â¡!?"`:()[\]{}]/;
+  var words = /[a-zÃ -ÃºÃ¼'â€™-]+\d*/;
+  var acronyms = /([a-z]\.){2,}/;
+  var eol = /\n/;
+  var other = /\S+/;
+
+  var opening = /^['"`Â¿Â¡([{\n]$/;
+  var closing = /^[.,;â€¦!?'"`:)\]}\n]$/;
+  var quotes = /^['"`]$/;
+
+  // Emoji regex from: https://github.com/mathiasbynens/emoji-regex
+  var emoji = /[\xA9\xAE\u203C\u2049\u2122\u2139\u2194-\u2199\u21A9\u21AA\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA\u24C2\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE\u2600-\u2604\u260E\u2611\u2614\u2615\u2618\u261D\u2620\u2622\u2623\u2626\u262A\u262E\u262F\u2638-\u263A\u2648-\u2653\u2660\u2663\u2665\u2666\u2668\u267B\u267F\u2692-\u2694\u2696\u2697\u2699\u269B\u269C\u26A0\u26A1\u26AA\u26AB\u26B0\u26B1\u26BD\u26BE\u26C4\u26C5\u26C8\u26CE\u26CF\u26D1\u26D3\u26D4\u26E9\u26EA\u26F0-\u26F5\u26F7-\u26FA\u26FD\u2702\u2705\u2708-\u270D\u270F\u2712\u2714\u2716\u271D\u2721\u2728\u2733\u2734\u2744\u2747\u274C\u274E\u2753-\u2755\u2757\u2763\u2764\u2795-\u2797\u27A1\u27B0\u27BF\u2934\u2935\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55\u3030\u303D\u3297\u3299]|\uD83C[\uDC04\uDCCF\uDD70\uDD71\uDD7E\uDD7F\uDD8E\uDD91-\uDD9A\uDE01\uDE02\uDE1A\uDE2F\uDE32-\uDE3A\uDE50\uDE51\uDF00-\uDF21\uDF24-\uDF93\uDF96\uDF97\uDF99-\uDF9B\uDF9E-\uDFF0\uDFF3-\uDFF5\uDFF7-\uDFFF]|\uD83D[\uDC00-\uDCFD\uDCFF-\uDD3D\uDD49-\uDD4E\uDD50-\uDD67\uDD6F\uDD70\uDD73-\uDD79\uDD87\uDD8A-\uDD8D\uDD90\uDD95\uDD96\uDDA5\uDDA8\uDDB1\uDDB2\uDDBC\uDDC2-\uDDC4\uDDD1-\uDDD3\uDDDC-\uDDDE\uDDE1\uDDE3\uDDEF\uDDF3\uDDFA-\uDE4F\uDE80-\uDEC5\uDECB-\uDED0\uDEE0-\uDEE5\uDEE9\uDEEB\uDEEC\uDEF0\uDEF3]|\uD83E[\uDD10-\uDD18\uDD80-\uDD84\uDDC0]|\uD83C\uDDFF\uD83C[\uDDE6\uDDF2\uDDFC]|\uD83C\uDDFE\uD83C[\uDDEA\uDDF9]|\uD83C\uDDFD\uD83C\uDDF0|\uD83C\uDDFC\uD83C[\uDDEB\uDDF8]|\uD83C\uDDFB\uD83C[\uDDE6\uDDE8\uDDEA\uDDEC\uDDEE\uDDF3\uDDFA]|\uD83C\uDDFA\uD83C[\uDDE6\uDDEC\uDDF2\uDDF8\uDDFE\uDDFF]|\uD83C\uDDF9\uD83C[\uDDE6\uDDE8\uDDE9\uDDEB-\uDDED\uDDEF-\uDDF4\uDDF7\uDDF9\uDDFB\uDDFC\uDDFF]|\uD83C\uDDF8\uD83C[\uDDE6-\uDDEA\uDDEC-\uDDF4\uDDF7-\uDDF9\uDDFB\uDDFD-\uDDFF]|\uD83C\uDDF7\uD83C[\uDDEA\uDDF4\uDDF8\uDDFA\uDDFC]|\uD83C\uDDF6\uD83C\uDDE6|\uD83C\uDDF5\uD83C[\uDDE6\uDDEA-\uDDED\uDDF0-\uDDF3\uDDF7-\uDDF9\uDDFC\uDDFE]|\uD83C\uDDF4\uD83C\uDDF2|\uD83C\uDDF3\uD83C[\uDDE6\uDDE8\uDDEA-\uDDEC\uDDEE\uDDF1\uDDF4\uDDF5\uDDF7\uDDFA\uDDFF]|\uD83C\uDDF2\uD83C[\uDDE6\uDDE8-\uDDED\uDDF0-\uDDFF]|\uD83C\uDDF1\uD83C[\uDDE6-\uDDE8\uDDEE\uDDF0\uDDF7-\uDDFB\uDDFE]|\uD83C\uDDF0\uD83C[\uDDEA\uDDEC-\uDDEE\uDDF2\uDDF3\uDDF5\uDDF7\uDDFC\uDDFE\uDDFF]|\uD83C\uDDEF\uD83C[\uDDEA\uDDF2\uDDF4\uDDF5]|\uD83C\uDDEE\uD83C[\uDDE8-\uDDEA\uDDF1-\uDDF4\uDDF6-\uDDF9]|\uD83C\uDDED\uD83C[\uDDF0\uDDF2\uDDF3\uDDF7\uDDF9\uDDFA]|\uD83C\uDDEC\uD83C[\uDDE6\uDDE7\uDDE9-\uDDEE\uDDF1-\uDDF3\uDDF5-\uDDFA\uDDFC\uDDFE]|\uD83C\uDDEB\uD83C[\uDDEE-\uDDF0\uDDF2\uDDF4\uDDF7]|\uD83C\uDDEA\uD83C[\uDDE6\uDDE8\uDDEA\uDDEC\uDDED\uDDF7-\uDDFA]|\uD83C\uDDE9\uD83C[\uDDEA\uDDEC\uDDEF\uDDF0\uDDF2\uDDF4\uDDFF]|\uD83C\uDDE8\uD83C[\uDDE6\uDDE8\uDDE9\uDDEB-\uDDEE\uDDF0-\uDDF5\uDDF7\uDDFA-\uDDFF]|\uD83C\uDDE7\uD83C[\uDDE6\uDDE7\uDDE9-\uDDEF\uDDF1-\uDDF4\uDDF6-\uDDF9\uDDFB\uDDFC\uDDFE\uDDFF]|\uD83C\uDDE6\uD83C[\uDDE8-\uDDEC\uDDEE\uDDF1\uDDF2\uDDF4\uDDF6-\uDDFA\uDDFC\uDDFD\uDDFF]|[#\*0-9]\u20E3/;
+
+  return tokenizer;
 }
